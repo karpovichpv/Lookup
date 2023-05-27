9bd54072ed0dd86b4c4c0e0f04b43a592989087c
Pavel Karpovich <karpovich.pavel@gmail.com>
2023-05-27 18:41:42 +0300

    Fill about button

---
+++ ViewModel/ViewModelAbout.cs
@@ -0,0 +1,27 @@
+﻿using Lookup.Commands;
+using System;
+using System.Diagnostics;
+
+namespace Lookup.ViewModel
+{
+    public class ViewModelAbout : ViewModelBase
+    {
+        public RelayCommand _runLink;
+        public RelayCommand RunLink
+        {
+            get
+            {
+                return _runLink
+                    ?? (new RelayCommand(
+                    obj => RunLinkRequest(),
+                    obj => true));
+            }
+        }
+
+        private void RunLinkRequest()
+        {
+            Uri uri = new Uri("https://pkbim.blogspot.com/");
+            Process.Start(new ProcessStartInfo(uri.AbsoluteUri));
+        }
+    }
+}
