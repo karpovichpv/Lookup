ff52dfde8f4c2470129e3c973dacc191d5143f7e
karpovichpv <karpovich.pavel@gmail.com>
2025-08-03 16:09:05 +0300

    (feat): add the ReportPropertyGetter class

---
+++ Lookup/TSProperties/ReportProperty/ReportPropertyGetter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Tekla.Structures.Model;
+using tsd = Tekla.Structures.Drawing;
+
+namespace Lookup.TSProperties.UserProperties
+{
+    internal class ReportPropertyGetter : IPropertyGetter
+    {
+        public IEnumerable<Property> GetAttributeList(object obj)
+        {
+            try
+            {
+                if (obj is ModelObject modelObj)
+                    return modelObj.Get();
+                if (obj is ProjectInfo projectObj)
+                    return projectObj.Get();
+                if (obj is tsd.Drawing drawingObj)
+                    return drawingObj.Get();
+                if (obj is tsd.ModelObject tsdModelObj)
+                    return tsdModelObj.Get();
+            }
+            catch
+            { }
+
+            return null;
+        }
+
+    }
+}
