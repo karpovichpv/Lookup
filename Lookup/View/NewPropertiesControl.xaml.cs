8e0efd50cd39ac080a5bec268b65067ef4769c43
karpovichpv <karpovich.pavel@gmail.com>
2025-09-20 16:22:01 +0300

    (feat): add new properties control

---
+++ Lookup/View/NewPropertiesControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Lookup.View
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class NewPropertiesControl : UserControl
+    {
+        public NewPropertiesControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
